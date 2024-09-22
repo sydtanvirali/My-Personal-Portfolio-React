acc87dad91fb9fffa70b60acf1db9625abbb11a4
Tanvir <sydtanvirali@gmail.com>
2024-09-22 15:19:22 +0530

    fix light/dark mode icon slow loading issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
   }, [darkMode]);
 
   return (
-    <div className="sticky w-full flex max-w-fit top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] p-8 py-3  items-center justify-center space-x-4">
+    <div className="fixed w-full flex max-w-fit top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] p-8 py-3  items-center justify-center space-x-4">
       {navItems.map((navItem, idx) => (
         <Link
           key={`link=${idx}`}
@@ -62,13 +62,27 @@ const Navbar = () => {
       ))}
       <a className="flex items-center cursor-pointer" onClick={toggleMode}>
         {darkMode ? (
-          <i className="material-symbols-outlined text-lg dark:text-neutral-50 dark:hover:text-neutral-300">
-            light_mode
-          </i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.125rem"
+            viewBox="0 -960 960 960"
+            width="1.125rem"
+            fill="#fafafa"
+            className="hover:fill-neutral-300"
+          >
+            <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+          </svg>
         ) : (
-          <i className="material-symbols-outlined text-lg text-neutral-600 hover:text-neutral-500">
-            dark_mode
-          </i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.125rem"
+            viewBox="0 -960 960 960"
+            width="1.125rem"
+            fill="#525252"
+            className="hover:fill-neutral-500"
+          >
+            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+          </svg>
         )}
       </a>
     </div>
